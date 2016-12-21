51e63debdb012f99d983486cf1992623d8159af7
sophiayangx <sopyang@nuevaschool.org>
2016-12-20 19:23:21 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -6,49 +6,25 @@ $("body").keydown(function(keydownEvent) {
 })
 
 
-//function sides(distance, n) {
-//  for (i = 0; i < n; i++) {
-//    goForward(100)
-//    turnLeft(n);
-//  }
-//}
-//
-//sides(3, 6)
-//
-//function sides(distance, n) {
-//  for (i = 0; i < n; i++) {
-//    goForward(100)
-//    turnLeftStar(n);
-//  }
-//}
-
-//sides(3, 6)
-
-//December 12th, Day 2
-
-$("body").keydown(function(keydownEvent) {
-  if (keydownEvent.keyCode === 39) {
-    goForward(100);
-  } 
-})
-
-$("body").keydown(function(keydownEvent) {
-  if (keydownEvent.keyCode === 38) {
-    turnUp()
+function sides(distance, n) {
+  for (i = 0; i < n; i++) {
     goForward(100)
-  } 
-})
+    turnLeft(n);
+  }
+}
 
-$("body").keydown(function(keydownEvent) {
-  if (keydownEvent.keyCode === 37) {
-    turnLeft();
-    goForward(100);
-  } 
-})
+sides(3, 6)
 
-$("body").keydown(function(keydownEvent) {
-  if (keydownEvent.keyCode === 40) {
-    turnDown();
+function sides(distance, n) {
+  for (i = 0; i < n; i++) {
+    goForward(100)
+    turnLeftStar(n);
+  }
+}
+
+sides(3, 6)
+
+
     goForward(100);
   } 
 })
