c19946ae4bc6aabc081e52c7a5234edb7a891f42
sophiayangx <sopyang@nuevaschool.org>
2016-12-23 22:10:23 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -6,21 +6,21 @@ $("body").keydown(function(keydownEvent) {
 
 $("body").keydown(function(keydownEvent) {
   if (keydownEvent.keyCode === 38) {
-    turnUp()
+    turnRight()
     goForward(100)
   } 
 })
 
-$("body").keydown(function(keydownEvent) {
-  if (keydownEvent.keyCode === 37) {
-    turnLeft();
-    goForward(100);
-  } 
-})
-
-$("body").keydown(function(keydownEvent) {
-  if (keydownEvent.keyCode === 40) {
-    turnDown();
-    goForward(100);
-  } 
-})
+//$("body").keydown(function(keydownEvent) {
+//  if (keydownEvent.keyCode === 37) {
+//    turnLeft();
+//    goForward(100);
+//  } 
+//})
+//
+//$("body").keydown(function(keydownEvent) {
+//  if (keydownEvent.keyCode === 40) {
+//    turnDown();
+//    goForward(100);
+//  } 
+//})
