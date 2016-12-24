9a3de8e6e9533022d95fa9031f97119c374163c9
sophiayangx <sopyang@nuevaschool.org>
2016-12-23 22:23:13 -0800

    Classadoo autocommit

--- turtles.js
+++ turtles.js
@@ -24,10 +24,10 @@ function goUp(distance) {
   var yIncrement = Math.cos(radians) * distance;
   var xIncrement = Math.sin(radians) * distance;        
 
-  var origPosition = {x: position.y, y: position.x}; 
+  var origPosition = {x: position.x, y: position.y}; 
   var origHeading = heading + 90;
 
-  position.x = position.x + xIncrement;
+//  position.x = position.x + xIncrement;
   position.y = position.y + yIncrement;
 
   turtle.animate({
